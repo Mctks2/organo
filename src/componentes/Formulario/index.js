40318a556e61f6731b6f99b21cdb55f93db2ad28
Thiago <mctks2@hotmail.com>
2024-06-04 20:26:39 -0300

    list of items

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,15 @@
 import "./Formulario.css";
 import { CampoTexto } from "../CampoTexto";
+import { ListaSuspensa } from "../ListaSuspensa";
 export function Formulario() {
   return (
     <section className="formulario">
       <form>
         <h2>Preencha os dados para criar o card do coloborador</h2>
-        <CampoTexto label="Nome" placeholder="digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="digite o endereço da imagem" />
-        <CampoTexto label="Time" />
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+        <ListaSuspensa label="Time" itens={["Programação", "Front-End", "Data Science", "Devops", "UX e Design", "Mobile"]}/>
       </form>
     </section>
   );
---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,13 @@
+import "./ListaSuspensa.css";
+export function ListaSuspensa(props) {
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+      <select>
+        {props.itens.map(item => {
+          return <option key={item}>{item}</option>;
+        })}
+      </select>
+    </div>
+  );
+}
